7fc192cc5c03b53550978aea82ded4267cdb274b
DeiviRep <patzivargasdavid@gmail.com>
2023-07-25 18:24:03 -0400

    Consumo api con ej: de YT FAZT con Framework VITE

--- src/components/Private/Biblioteca.jsx
+++ src/components/Private/Biblioteca.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './styles-modules/Biblioteca.module.css'
+import CharacterList from '../character/CharacterList'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Biblioteca() {
   return (
@@ -8,6 +10,9 @@ function Biblioteca() {
       <div className={styles.container}>
         <h1 className={styles.title}>Título del componente</h1>
         <p>Contenido del componente...</p>
+      </div>
+      <div className='bg-dark text-white container p-2'>
+        <CharacterList />
       </div>
     </>
   )
